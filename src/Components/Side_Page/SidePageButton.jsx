d7c24463d54537fa48044ab371a3b11e529f3a9e
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-09-06 20:56:23 +0100

    added hover box to side page buttons

--- src/Components/Side_Page/SidePageButton.jsx
+++ src/Components/Side_Page/SidePageButton.jsx
@@ -3,7 +3,9 @@ export const SidePageButton = (props) =>
     const buttonName = props.name;
     return(
         <div id="Side_Page_Button">
-            {buttonName}
+            <div id="Hover_Box">
+                {buttonName}
+            </div>
         </div>
     )
 }
