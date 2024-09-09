9ecf410aa3f0bc2e0a48265493bd1c891307a15a
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-09-09 18:30:35 +0100

    dynamically rendered components on click of sidePageButton

--- src/Components/Side_Page/SidePage.jsx
+++ src/Components/Side_Page/SidePage.jsx
@@ -1,14 +1,15 @@
 import { SidePageButton } from "./SidePageButton"
 
-export const SidePage = () =>
+export const SidePage = (props) =>
 {
+    const setViewedComponentString = props.setViewedComponentString
     return(
         <div id="Side_Page_Container" className='Container_Box'>
             <div id="Side_Page_Button_Container">
-                <SidePageButton name={"Home"}/>
-                <SidePageButton name={"Experience"}/>
-                <SidePageButton name={"Projects"}/>
-                <SidePageButton name={"Contacts"}/>
+                <SidePageButton name={"Home"} setViewedComponentString = {setViewedComponentString}/>
+                <SidePageButton name={"Experience"} setViewedComponentString = {setViewedComponentString}/>
+                <SidePageButton name={"Projects"} setViewedComponentString = {setViewedComponentString}/>
+                <SidePageButton name={"Contacts"} setViewedComponentString = {setViewedComponentString}/>
             </div>
         </div>
     )
--- src/Components/Side_Page/SidePageButton.jsx
+++ src/Components/Side_Page/SidePageButton.jsx
@@ -1,8 +1,16 @@
 export const SidePageButton = (props) =>
 {
     const buttonName = props.name;
+    const setViewedComponentString = props.setViewedComponentString
+
+
+    function switchComponent(event)
+    {
+        event.preventDefault()
+        setViewedComponentString(props.name)
+    }
     return(
-        <div id="Side_Page_Button">
+        <div id="Side_Page_Button" onClick={(event) => {switchComponent(event)}}>
             <div id="Hover_Box">
                 {buttonName}
             </div>
