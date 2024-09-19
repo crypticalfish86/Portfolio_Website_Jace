bbf7f4d5ecce4ef8d9f0155c940ce06633dc3de4
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-09-19 22:58:07 +0100

    variable experience/projects page set up

--- src/Components/Main_Page/Experience_And_Project_Page.jsx
+++ src/Components/Main_Page/Experience_And_Project_Page.jsx
@@ -1,9 +1,29 @@
 /*This div should display with the content of whatever is in the Experience_And_Projects_Context*/
 
+import { useEffect, useState } from "react";
+
 export const ExperienceAndProjectsPage = (props) =>
 {
     const objectToDisplay = props.objectToDisplay;
+
+    const [currentDivHTML, setCurrentDivHTML] = useState(<div>Experience And Projects Page</div>)
+    
+    useEffect( () =>
+        {
+            if(objectToDisplay.objectType === "Experience")
+            {
+                setCurrentDivHTML(<div>Experience Page</div>)
+            }
+            else if(objectToDisplay.objectType === "Project")
+            {
+                setCurrentDivHTML(<div>Projects Page</div>)
+            }
+        }, [objectToDisplay]
+    )
+
     return(
-        <div>Experience And Projects Page</div>
+        <div id="Whole_Experience_And_Projects_Page_Container">
+                {currentDivHTML}
+        </div>
     )
 }
