03476b1e0fa4064b4f67cb40f27aaaccf7104233
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-09-20 03:17:19 +0100

    basic html setup for experience page

--- src/Components/Main_Page/Experience_And_Project_Page.jsx
+++ src/Components/Main_Page/Experience_And_Project_Page.jsx
@@ -1,4 +1,20 @@
-/*This div should display with the content of whatever is in the Experience_And_Projects_Context*/
+/*
+    This div should display with the content of whatever is in the Experience_And_Projects_Context
+    
+    ExperienceObject looks like this:
+           const experienceObject = 
+            {
+                objectType: "Experience",//experience objects ALWAYS have this type to differentiate from project/work objects
+                companyName: companyName,
+                jobName: jobName,
+                date: date,
+                jobDescription: jobDescription
+            }
+    
+    ProjectObjects look like this (for now, this is subject to change):
+
+    {objectType : "Project", description : txtFileContent, images : images}
+*/
 
 import { useEffect, useState } from "react";
 
@@ -8,11 +24,21 @@ export const ExperienceAndProjectsPage = (props) =>
 
     const [currentDivHTML, setCurrentDivHTML] = useState(<div>Experience And Projects Page</div>)
     
+    /*If the Object received has the object type "Experience" format html to display an experience page, otherwise format page to look like a projects page*/
     useEffect( () =>
         {
             if(objectToDisplay.objectType === "Experience")
             {
-                setCurrentDivHTML(<div>Experience Page</div>)
+                setCurrentDivHTML(
+                    <div id="Experience_Page">
+                        <div id="Experience_Title">
+                            <div id="Experience_Company_Name">{objectToDisplay.companyName}</div>
+                        </div>
+                        <div id="Experience_Job_Title">{objectToDisplay.jobName}</div>
+                        <div id="Experience_Date">{objectToDisplay.date}</div>
+                        <div id="Experience_Description">{objectToDisplay.jobDescription}</div>
+                    </div>
+                )
             }
             else if(objectToDisplay.objectType === "Project")
             {
@@ -22,7 +48,7 @@ export const ExperienceAndProjectsPage = (props) =>
     )
 
     return(
-        <div id="Whole_Experience_And_Projects_Page_Container">
+        <div id="Whole_Experience_And_Projects_Page_Container" className="Container_Box">
                 {currentDivHTML}
         </div>
     )
