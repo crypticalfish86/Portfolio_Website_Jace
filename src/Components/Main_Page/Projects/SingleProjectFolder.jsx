e8da8a0336b2d37592623a76f578994735b547cd
Jace Weerawardena <jasonweerawardena@gmail.com>
2024-10-03 21:14:24 +0100

    small readability changes to singleprojectfolder

--- src/Components/Main_Page/Projects/SingleProjectFolder.jsx
+++ src/Components/Main_Page/Projects/SingleProjectFolder.jsx
@@ -1,17 +1,19 @@
 import { useContext, useEffect, useMemo, useState } from "react";
 import { ExperienceAndProjectsContext } from "../../Experience_And_Projects_Context";
 
+/*
+    This renders the single project folders as well as loads their content into the "ExperienceAndProjects" page to display when one of these components is clicked
+*/
+
+
 export const SingleProjectFolder = (props) =>
 {
 
+    
+    //Use the asynchronous function "fetch" to turn the .txt filepath into the content of that file
     const txtFile = props.txtFile; //The file containing the work object description
     const images = props.images; //the images of the work object
-
-    const {currentProjectOrExperienceObject, setProjectOrExperienceCurrentObject} = useContext(ExperienceAndProjectsContext);//The context to pass the project object to so app can change the main page if folder is clicked
-
-    const [txtFileContent, setFileContent] = useState('');//state to store the .txt file content
-
-    //Use the asynchronous function "fetch" to turn the .txt filepath into the content of that file
+    const [txtFileContent, setFileContent] = useState('');//state to store the raw .txt file content as a string
     useEffect( () =>
         {
             fetch(txtFile) //read the file
@@ -22,42 +24,42 @@ export const SingleProjectFolder = (props) =>
     )
     
 
-    function getSpecificContentAfterStringMarker(stringMarker) //this child function returns the specific content after a stringMarker e.g. COMPANY_NAME and before the next stringMarker e.g.JOB_Name
-    {
-        const fullContent = txtFileContent;
-        const startIndex = txtFileContent.indexOf(stringMarker);//returns the index of the start of the stringMarker in the content
-
-        if(startIndex !== -1) //Check to ensure the stringMarker has been found before proceeding
-        {
-            const entireStringAfterStringMarker = fullContent.slice(startIndex + stringMarker.length)
-            const nextStringMarkerInContent = entireStringAfterStringMarker.match(/\b\w*[A-Z]{3,}\w*\b/) //Finds the next word containing more than 3 capital letters in it
-
-            if (nextStringMarkerInContent)
-            {
-                return entireStringAfterStringMarker.slice(0, entireStringAfterStringMarker.indexOf(nextStringMarkerInContent[0])).trim() //returns all the content after stringMarker but before next stringMarker with any whitespace trimmed off
-            }
-            else
-            {
-                return entireStringAfterStringMarker;
-            }
-        }
-        else
-        {
-            return "Error string not found"
-        }
-    }
-
-
     const projectObject = useMemo( () => //this is the project object to be sent up to the parent context allowing us to trigger the page change to this specific project
         {
             return {objectType : "Project",title: getSpecificContentAfterStringMarker("TITLE:"), date: getSpecificContentAfterStringMarker("DATE:"), technologiesUsed: getSpecificContentAfterStringMarker("TECHNOLOGIES_USED:"),  description : getSpecificContentAfterStringMarker("DESCRIPTION:"), images : images}
         }, [txtFileContent]
     )
+        function getSpecificContentAfterStringMarker(stringMarker) //this child function returns the specific content after a stringMarker e.g. COMPANY_NAME and before the next stringMarker e.g.JOB_Name
+        {
+            const fullContent = txtFileContent;
+            const startIndex = txtFileContent.indexOf(stringMarker);//returns the index of the start of the stringMarker in the content
 
-   
+            if(startIndex !== -1) //Check to ensure the stringMarker has been found before proceeding
+            {
+                const entireStringAfterStringMarker = fullContent.slice(startIndex + stringMarker.length)
+                const nextStringMarkerInContent = entireStringAfterStringMarker.match(/\b\w*[A-Z]{3,}\w*\b/) //Finds the next word containing more than 3 capital letters in it
+
+                if (nextStringMarkerInContent)
+                {
+                    return entireStringAfterStringMarker.slice(0, entireStringAfterStringMarker.indexOf(nextStringMarkerInContent[0])).trim() //returns all the content after stringMarker but before next stringMarker with any whitespace trimmed off
+                }
+                else
+                {
+                    return entireStringAfterStringMarker;
+                }
+            }
+            else
+            {
+                return "Error string not found"
+            }
+        }
+
+
+
+
+    const {currentProjectOrExperienceObject, setProjectOrExperienceCurrentObject} = useContext(ExperienceAndProjectsContext);//The context to pass the project object to so app can change the main page if folder is clicked
     function openProjectPage(event)
     {
-        console.log(projectObject)
         event.preventDefault();
         setProjectOrExperienceCurrentObject(projectObject)
     }
